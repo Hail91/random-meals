f9ebdd26bcb2434403a90f10112bbcbcb740c6b0
Aaron <aarongillies88@gmail.com>
2020-01-25 18:09:24 -0500

    Built out recipe card with relevant data, minor styling additions

--- random-meals/src/components/Main.js
+++ random-meals/src/components/Main.js
@@ -18,18 +18,39 @@ const Main = () => {
     }, []);
 
     console.log(meal)
-    
+  
     return (
     <div>
       <NavBar />
-      <div className="recipe-cont">
-        <h1>{meal.map(info => {
+        {meal.map(info => {
             return (
-            <h1>{info.strMeal}</h1>
+            <div className="main-cont">
+                <RecipeCard data={meal}/>
+                <h2>Ingredients</h2>
+                    {info.strMeasure1 && info.strIngredient1 ? <li>{info.strMeasure1} - {info.strIngredient1}</li> : null}
+                    {info.strMeasure2 && info.strIngredient2 ? <li>{info.strMeasure2} - {info.strIngredient2}</li> : null}
+                    {info.strMeasure3 && info.strIngredient3 ? <li>{info.strMeasure3} - {info.strIngredient3}</li> : null}
+                    {info.strMeasure4 && info.strIngredient4 ? <li>{info.strMeasure4} - {info.strIngredient4}</li> : null}
+                    {info.strMeasure5 && info.strIngredient5 ? <li>{info.strMeasure5} - {info.strIngredient5}</li> : null}
+                    {info.strMeasure6 && info.strIngredient6 ? <li>{info.strMeasure6} - {info.strIngredient6}</li> : null}
+                    {info.strMeasure7 && info.strIngredient7 ? <li>{info.strMeasure7} - {info.strIngredient7}</li> : null}
+                    {info.strMeasure8 && info.strIngredient8 ? <li>{info.strMeasure8} - {info.strIngredient8}</li> : null}
+                    {info.strMeasure9 && info.strIngredient9 ? <li>{info.strMeasure9} - {info.strIngredient9}</li> : null}
+                    {info.strMeasure10 && info.strIngredient10 ? <li>{info.strMeasure10} - {info.strIngredient10}</li> : null}
+                    {info.strMeasure11 && info.strIngredient11 ? <li>{info.strMeasure11} - {info.strIngredient11}</li> : null}
+                    {info.strMeasure12 && info.strIngredient12 ? <li>{info.strMeasure12} - {info.strIngredient12}</li> : null}
+                    {info.strMeasure13 && info.strIngredient13 ? <li>{info.strMeasure13} - {info.strIngredient13}</li> : null}
+                    {info.strMeasure14 && info.strIngredient14 ? <li>{info.strMeasure14} - {info.strIngredient14}</li> : null}
+                    {info.strMeasure15 && info.strIngredient15 ? <li>{info.strMeasure15} - {info.strIngredient15}</li> : null}
+                    {info.strMeasure16 && info.strIngredient16 ? <li>{info.strMeasure16} - {info.strIngredient16}</li> : null}
+                    {info.strMeasure17 && info.strIngredient17 ? <li>{info.strMeasure17} - {info.strIngredient17}</li> : null}
+                    {info.strMeasure18 && info.strIngredient18 ? <li>{info.strMeasure18} - {info.strIngredient18}</li> : null}
+                    {info.strMeasure19 && info.strIngredient19 ? <li>{info.strMeasure19} - {info.strIngredient19}</li> : null}
+                    {info.strMeasure20 && info.strIngredient20 ? <li>{info.strMeasure20} - {info.strIngredient20}</li> : null}         
+                </div>
             )
-        })}</h1>
+        })}
       </div>
-    </div>
     );
 };
 
--- random-meals/src/components/RecipeCard.js
+++ random-meals/src/components/RecipeCard.js
@@ -15,10 +15,6 @@ import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
-import { Button } from '@material-ui/core';
-
-// Axios
-import axios from 'axios';
 
 const useStyles = makeStyles(theme => ({
   card: {
@@ -51,82 +47,78 @@ const RecipeCard = (props) => {
     setExpanded(!expanded);
   };
 
-  console.log(props)
+  console.log(props.data)
 
   return (
-    <Card className={classes.card}>
-      <CardHeader
-        avatar={
-          <Avatar aria-label="recipe" className={classes.avatar}>
-            R
-          </Avatar>
-        }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title="Hello"
-        subheader="September 14, 2016"
-      />
-      <CardMedia
-        className={classes.media}
-        image="/static/images/cards/paella.jpg"
-        title="Paella dish"
-      />
-      <CardContent>
-        <Typography variant="body2" color="textSecondary" component="p">
-          This impressive paella is a perfect party dish and a fun meal to cook together with your
-          guests. Add 1 cup of frozen peas along with the mussels, if you like.
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
-        <IconButton
-          className={clsx(classes.expand, {
-            [classes.expandOpen]: expanded,
-          })}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </IconButton>
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography paragraph>Method:</Typography>
-          <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-            minutes.
-          </Typography>
-          <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-            heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-            browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
-            and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
-            pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
-            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-          </Typography>
-          <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
-            without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
-            medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
-            again without stirring, until mussels have opened and rice is just tender, 5 to 7
-            minutes more. (Discard any mussels that don’t open.)
-          </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then serve.
-          </Typography>
-        </CardContent>
-      </Collapse>
-    </Card>
+      props.data.map(info => {
+          return (
+            <Card className={classes.card}>
+            <CardHeader
+              avatar={
+                <Avatar aria-label="recipe" className={classes.avatar}>
+                  R
+                </Avatar>
+              }
+              action={
+                <IconButton aria-label="settings">
+                  <MoreVertIcon />
+                </IconButton>
+              }
+              title={info.strMeal}
+              subheader="January 25, 2020"
+            />
+            <CardMedia
+              className={classes.media}
+              image={info.strMealThumb}
+              title="Paella dish"
+            />
+            <CardContent>
+              <Typography>
+                  Category/Area
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                {info.strCategory}/ {info.strArea}
+              </Typography>
+            </CardContent>
+            <CardActions disableSpacing>
+              <IconButton aria-label="add to favorites">
+                <FavoriteIcon />
+              </IconButton>
+              <IconButton aria-label="share">
+                <ShareIcon />
+              </IconButton>
+              <IconButton
+                className={clsx(classes.expand, {
+                  [classes.expandOpen]: expanded,
+                })}
+                onClick={handleExpandClick}
+                aria-expanded={expanded}
+                aria-label="show more"
+              >
+                <ExpandMoreIcon />
+              </IconButton>
+            </CardActions>
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+              <CardContent>
+                <Typography paragraph>Instructions:</Typography>
+                <Typography paragraph>
+                  {info.strInstructions}
+                </Typography>
+              </CardContent>
+              <CardContent>
+              <div class="videoWrapper">
+                <iframe 
+                  title="recipe"
+                  width="420" 
+                  height="315"
+                  src={`https://www.youtube.com/embed/${info.strYoutube.slice(-11)}`}>
+				        </iframe>
+			        </div>
+              </CardContent>
+            </Collapse>
+          </Card>
+          )
+      })
   );
 }
-
 export default RecipeCard;
